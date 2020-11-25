076ebea7d4ed1d9cd0baa67f01a215b98645093e
Sanjay-RD <rishidevsanjay9@gmail.com>
2020-11-25 16:57:25 +0545

    HTTP Request and Updating State

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/user/Users";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    // without using async await
+    // axios
+    //   .get("https://api.github.com/users")
+    //   .then((res) => this.setState({ users: res.data, loading: false }));
+
+    // using async await
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data });
+  }
   render() {
     return (
       <div className="App">
         {/* passing props in navbar */}
         <Navbar title="Navbar" icon="fa fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
