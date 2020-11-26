40512a84ccf1f808817113f0854faa4c8de3ff9f
Sanjay-RD <rishidevsanjay9@gmail.com>
2020-11-26 20:44:37 +0545

    Passing Props Up & Search

--- src/App.js
+++ src/App.js
@@ -10,23 +10,36 @@ class App extends Component {
     users: [],
     loading: false,
   };
-  async componentDidMount() {
-    // console.log(process.env.REACT_APP_GITHUB_CLIENT_CLIENT);
+
+  // async componentDidMount() {
+  //   // console.log(process.env.REACT_APP_GITHUB_CLIENT_CLIENT);
+  //   this.setState({ loading: true });
+
+  //   // using async await
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_CLIENT}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
     // using async await
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_CLIENT}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_CLIENT}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className="App">
         {/* passing props in navbar */}
         <Navbar title="Navbar" icon="fa fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
